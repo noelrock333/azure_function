bdb41d6a13022ce93f5a09e6e10c1b0bef3116af
Noel Escobedo <noel@michelada.io>
2018-10-09 20:58:10 +0000

    Deploy instructions

---
+++ README.md
@@ -0,0 +1,6 @@
+### Deploy function
+- Install node 10.6.0
+- Install Visual Studio Code
+- Install `Azure Functions` plugin for VSCode and following the instructions
+- Create a new setting on Application Settings and use a special name for your Service Bus connection string, that name should be the same as the one in your `function.json` file in the `connection` property
+- Deploy your function on the arrow up
--- ServiceBusQueueTriggerJS1/index.js
+++ ServiceBusQueueTriggerJS1/index.js
@@ -1,8 +1,5 @@
 module.exports = async function(context, mySbMsg) {
-    context.log('JavaScript ServiceBus queue trigger function processed message', mySbMsg);
-    context.log('Node.js ServiceBus queue trigger function processed message', mySbMsg);
-    context.log('EnqueuedTimeUtc =', context.bindingData.enqueuedTimeUtc);
-    context.log('DeliveryCount =', context.bindingData.deliveryCount);
+    context.log('Message =', mySbMsg);
     context.log('MessageId =', context.bindingData.messageId);
     context.done();
 };
